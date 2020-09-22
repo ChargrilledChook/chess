96c1930add561d829ea00ec2e024eb36cb5737ed
chook <dylanw212@gmail.com>
2020-09-22 16:13:23 +1000

    testing scripts

--- main.rb
+++ main.rb
@@ -1,12 +1,15 @@
-require_relative "lib/icons"
+require_relative "lib/piece_icons"
 require_relative "lib/board"
 require_relative "lib/piece"
 require_relative "lib/game"
-
+require_relative "lib/king"
 #pp Board.new.grid
 
-game = Game.new(players: [:white, :black])
+# game = Game.new(players: %i[white black])
 
-puts game.players
-game.swap_players
-puts game.players
+# puts game.players
+# game.swap_players
+# puts game.players
+# game.swap_players
+
+puts King.new.icon
