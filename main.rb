b7226d91d39733953157f4331185c2e2f5482dbd
chook <dylanw212@gmail.com>
2020-09-22 10:53:30 +1000

    small script to check proper icon output

--- main.rb
+++ main.rb
@@ -4,18 +4,3 @@ require_relative "lib/piece"
 require_relative "lib/game"
 
 #pp Board.new.grid
-
-rook = Piece.new(colour: 'Black', type: 'Rook')
-
-puts rook.black_rook
-puts rook.white_rook
-puts rook.white_bishop
-puts rook.black_bishop
-puts rook.white_knight
-puts rook.black_knight
-puts rook.white_pawn
-puts rook.black_pawn
-puts rook.white_queen
-puts rook.black_queen
-puts rook.white_king
-puts rook.black_king
