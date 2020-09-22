e99622aeb93d48401481f09bc19ff6a503fc93a1
chook <dylanw212@gmail.com>
2020-09-22 16:12:32 +1000

    reorganise file

--- icon_script.rb
+++ icon_script.rb
@@ -1,4 +1,4 @@
-require_relative "lib/icons"
+require_relative "lib/piece_icons"
 require_relative "lib/board"
 require_relative "lib/piece"
 require_relative "lib/game"
@@ -7,15 +7,21 @@ require_relative "lib/game"
 
 rook = Piece.new(colour: 'Black', type: 'Rook')
 
-puts rook.black_rook
+
 puts rook.white_rook
+puts rook.black_rook
+puts
 puts rook.white_bishop
 puts rook.black_bishop
+puts
 puts rook.white_knight
 puts rook.black_knight
+puts
 puts rook.white_pawn
 puts rook.black_pawn
+puts
 puts rook.white_queen
 puts rook.black_queen
+puts
 puts rook.white_king
 puts rook.black_king
