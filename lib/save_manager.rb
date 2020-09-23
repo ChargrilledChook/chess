029eac10f0c6a5fb41d505a2cc9957c7067dad02
chook <dylanw212@gmail.com>
2020-09-23 18:05:33 +1000

    old implemtation added

---
+++ lib/save_manager.rb
@@ -0,0 +1,24 @@
+# Saves and loads games / serializes objects
+# This implementation is from an old project and could do with a pass for best practices
+module SaveManager
+  def default_save
+    "save.txt"
+  end
+
+  def save_message
+    "game saved, exiting program"
+  end
+
+  def save_game(round)
+    yaml = YAML.dump(round)
+    file = File.new(default_save, 'w+')
+    file.puts(yaml)
+    file.close
+    puts save_message
+    exit
+  end
+
+  def load_save
+    YAML.load(File.read(default_save))
+  end
+end
