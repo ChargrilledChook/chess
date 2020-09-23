9d639e55253cc4fd567b235380cfc1d6f518b158
chook <dylanw212@gmail.com>
2020-09-23 18:05:19 +1000

    basic tests for game

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,6 +1,20 @@
 require_relative "../lib/game"
 
 describe Game do
-  subject(:game) { described_class.new }
+  subject(:game) { described_class.new(round: round) }
   let(:round) { double("round") }
+
+  describe "#play" do
+    it "loops correctly" do
+      allow(round).to receive(:gameover?).and_return(false, true)
+      expect(round).to receive(:play).once
+      game.play
+    end
+
+    it "works with a longer loop" do
+      allow(round).to receive(:gameover?).and_return(false, false, false, false, false, true)
+      expect(round).to receive(:play).exactly(5).times
+      game.play
+    end
+  end
 end
