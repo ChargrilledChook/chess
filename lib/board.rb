0d53f8198844c798496680cae96fef1f51f74239
chook <dylanw212@gmail.com>
2020-09-24 16:46:30 +1000

    some board.to_s experiments

--- lib/board.rb
+++ lib/board.rb
@@ -1,8 +1,23 @@
 # Holds piece objects
 class Board
+  include PieceIcons # Currently for testing
+
   attr_accessor :grid # Change to reader
 
   def initialize
-    @grid = Array.new(8) { Array.new(8, 0) }
+    @grid = Array.new(8) { Array.new(8, "   ") }
+  end
+
+  def to_s
+    (1..8).each { |num| print " #{num} " }
+    print "\n"
+    grid.each do |line|
+      line.each { |cell| print cell.bg_cyan }
+      print "\n"
+    end
+  end
+
+  def render_proto
+    print " #{white_pawn} ".bg_blue + " #{white_pawn} ".bg_black + " #{black_pawn} ".bg_blue + " #{black_pawn} ".bg_black
   end
 end
--- main.rb
+++ main.rb
@@ -6,6 +6,6 @@ require_relative "lib/piece"
 require_relative "lib/game"
 require_relative "lib/king"
 
-puts "Welcome message here + rules etc".red.bg_magenta
+#puts "Welcome message here + rules etc".red.bg_magenta
 
-Game.new.new_game
+#Game.new.new_game
