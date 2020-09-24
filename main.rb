c2fc86f96e0ffe1371613a4ab4096d32a9b6e544
chook <dylanw212@gmail.com>
2020-09-24 14:42:45 +1000

    add colour monkey patch + settings

--- main.rb
+++ main.rb
@@ -1,3 +1,4 @@
+require_relative "lib/colour_patch"
 require_relative "lib/save_manager"
 require_relative "lib/piece_icons"
 require_relative "lib/board"
@@ -5,6 +6,6 @@ require_relative "lib/piece"
 require_relative "lib/game"
 require_relative "lib/king"
 
-puts "Welcome message here + rules etc"
+puts "Welcome message here + rules etc".red.bg_magenta
 
 Game.new.new_game
