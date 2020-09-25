9463999ad8f6ab6520095db1d8d2a2d424cde088
chook <dylanw212@gmail.com>
2020-09-25 13:38:39 +1000

    change require order, prob uneeded

--- main.rb
+++ main.rb
@@ -1,6 +1,6 @@
+require_relative "lib/piece_icons"
 require_relative "lib/colour_patch"
 require_relative "lib/save_manager"
-require_relative "lib/piece_icons"
 require_relative "lib/board"
 require_relative "lib/piece"
 require_relative "lib/game"
