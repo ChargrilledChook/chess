eb2e19836d7470ff5b22a2a3d8b821c61b880525
chook <dylanw212@gmail.com>
2020-09-28 12:30:01 +1000

    add skeleton for round algo

--- lib/round.rb
+++ lib/round.rb
@@ -3,9 +3,9 @@ class Round
   attr_reader :players, :board, :ref
 
   def initialize(
-    players: nil, # [Player.new(:white), Player.new(:black)]
-    board: nil,   # Board.new
-    ref: nil      # Ref.new
+    players: [Player.new(colour: :white), Player.new(colour: :black)],
+    board: Board.new,
+    ref: nil # Ref.new
   )
 
     @players = players
@@ -13,7 +13,32 @@ class Round
     @ref = ref
   end
 
+  def play
+    # TODO
+    move = players.first.input_move
+    board.place_move(move)
+    clear_console
+    draw_console
+    swap_players
+  end
+
+  def game_over?
+    # TODO
+    false
+  end
+
+  def draw_console
+    # TODO: Add move history, taken pieces etc
+    board.render_board
+  end
+
   def swap_players
     @players.rotate!
+  end
+
+  private
+
+  def clear_console
+    puts "\e[H\e[2J"
   end
 end
