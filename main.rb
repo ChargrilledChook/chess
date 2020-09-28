bac34f40dffe49d49dfec17b31d8b41aea34ffea
chook <dylanw212@gmail.com>
2020-09-28 12:28:22 +1000

    update to integration testing script

--- main.rb
+++ main.rb
@@ -5,12 +5,8 @@ require_relative "lib/save_manager"
 require_relative "lib/board"
 require_relative "lib/player"
 require_relative "lib/piece"
+require_relative "lib/round"
 require_relative "lib/game"
 require_relative "lib/king"
 
-#puts "Welcome message here + rules etc".red.bg_magenta
-
-#Game.new.new_game
-
-player = Player.new(colour: :white)
-puts player.input_move
+Game.new.play
