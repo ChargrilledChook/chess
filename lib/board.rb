913231340c4ba425d0d0432d680aad25624d7b5c
chook <dylanw212@gmail.com>
2020-09-28 22:27:51 +1000

    added methods to fill a default board with correct piece arrangement

--- lib/board.rb
+++ lib/board.rb
@@ -2,11 +2,11 @@
 class Board
   EMPTY_CELL = "   ".freeze
 
-  attr_reader :grid # Change to reader - currently for testing.
+  attr_reader :grid
 
   def initialize
     @grid = Array.new(8) { Array.new(8, EMPTY_CELL) }
-    place_default_pawns
+    place_default_pieces
   end
 
   def place_move(co_ords)
@@ -25,11 +25,6 @@ class Board
 
   def format_input(co_ords)
     co_ords.map(&:to_i)
-  end
-
-  def place_default_pawns
-    @grid[1].each_index { |cell| @grid[1][cell] = Pawn.new(colour: :black) }
-    @grid[6].each_index { |cell| @grid[6][cell] = Pawn.new(colour: :white) }
   end
 
   # HACK: Refactor
@@ -51,5 +46,39 @@ class Board
       black_cell = !black_cell
       print "\n"
     end
+  end
+
+  # Piece placement should possible be extracted to a module but it's fine here for now
+  def place_default_pieces
+    place_default_pawns
+    place_default_black_pieces
+    place_default_white_pieces
+  end
+
+  def place_default_pawns
+    @grid[1].each_index { |cell| @grid[1][cell] = Pawn.new(colour: :black) }
+    @grid[6].each_index { |cell| @grid[6][cell] = Pawn.new(colour: :white) }
+  end
+
+  def place_default_black_pieces
+    @grid[0][0] = Rook.new(colour: :black)
+    @grid[0][1] = Bishop.new(colour: :black)
+    @grid[0][2] = Knight.new(colour: :black)
+    @grid[0][3] = Queen.new(colour: :black)
+    @grid[0][4] = King.new(colour: :black)
+    @grid[0][5] = Knight.new(colour: :black)
+    @grid[0][6] = Bishop.new(colour: :black)
+    @grid[0][7] = Rook.new(colour: :black)
+  end
+
+  def place_default_white_pieces
+    @grid[7][0] = Rook.new(colour: :white)
+    @grid[7][1] = Bishop.new(colour: :white)
+    @grid[7][2] = Knight.new(colour: :white)
+    @grid[7][3] = Queen.new(colour: :white)
+    @grid[7][4] = King.new(colour: :white)
+    @grid[7][5] = Knight.new(colour: :white)
+    @grid[7][6] = Bishop.new(colour: :white)
+    @grid[7][7] = Rook.new(colour: :white)
   end
 end
