50fe815c3b3a38347164e4294ba8237ac9ba77c9
chook <dylanw212@gmail.com>
2020-09-29 13:31:10 +1000

    add ref class with notation converter

---
+++ lib/referee.rb
@@ -0,0 +1,22 @@
+class Referee
+  def initialize
+    # TODO
+  end
+
+  def convert_notation(notation)
+    result = notation.each_char.map.with_index do |elt, idx|
+      idx.even? ? convert_column(elt) : convert_row(elt)
+    end
+    starting = [result[1], result[0]]
+    ending = [result[3], result[2]]
+    [starting, ending]
+  end
+
+  def convert_column(letter)
+    letter.tr('a-h', '0-7').to_i
+  end
+
+  def convert_row(digit)
+    (digit.to_i - 8).abs
+  end
+end
