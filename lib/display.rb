7b4b3729b35f27647e724562b9028e4e1e08568d
chook <dylanw212@gmail.com>
2020-10-02 15:43:27 +1000

    add title screen messages

--- lib/display.rb
+++ lib/display.rb
@@ -3,4 +3,24 @@ module Display
   def move_prompt_msg
     "\n#{colour.capitalize}, it's your move: "
   end
+
+  def title_art
+    <<~HERE
+
+      ▄████████    ▄█    █▄       ▄████████    ▄████████    ▄████████
+      ███    ███   ███    ███     ███    ███   ███    ███   ███    ███
+      ███    █▀    ███    ███     ███    █▀    ███    █▀    ███    █▀
+      ███         ▄███▄▄▄▄███▄▄  ▄███▄▄▄       ███          ███
+      ███        ▀▀███▀▀▀▀███▀  ▀▀███▀▀▀     ▀███████████ ▀███████████
+      ███    █▄    ███    ███     ███    █▄           ███          ███
+      ███    ███   ███    ███     ███    ███    ▄█    ███    ▄█    ███
+      ████████▀    ███    █▀      ██████████  ▄████████▀   ▄████████▀
+
+
+    HERE
+  end
+
+  def welcome_msg
+    "\nWelcome to Chess! Enter 1 for a new game or 2 to load your saved game. "
+  end
 end
