7356664729d2ef789a14459ed933f113cd011614
chook <dylanw212@gmail.com>
2020-10-02 11:50:57 +1000

    most player tests complete

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,51 +1,71 @@
-require_relative "lib/player"
+require_relative "../lib/display"
+require_relative "../lib/player"
 
 describe Player do
-  subject(:player) { described_class.new(:white) }
+  subject(:player) { described_class.new(colour: :white) }
 
-  describe "#input_move" do
+  describe "#valid_move?" do
     context "with valid notation input" do
-      it "sends the correct message" do
-        # TODO
+      it "is correct" do
+        valid_input = "a1h8"
+        result = player.valid_input?(valid_input)
+        expect(result).to be true
       end
       it "is correct with different input" do
-        # TODO
+        valid_input = "b2g7"
+        result = player.valid_input?(valid_input)
+        expect(result).to be true
       end
       it "ignores case" do
-        # TODO
+        valid_input = "C3F6"
+        result = player.valid_input?(valid_input)
+        expect(result).to be true
       end
     end
 
     context "with valid keyword input" do
-      it "sends the correct message" do
-        # TODO
+      it "is correct" do
+        valid_input = "save"
+        result = player.valid_input?(valid_input)
+        expect(result).to be true
       end
       it "ignores case" do
-        # TODO
+        valid_input = "SAVE"
+        result = player.valid_input?(valid_input)
+        expect(result).to be true
       end
     end
 
     context "with invalid input" do
-      it "reprompts numbers" do
-        # TODO
+      it "rejects words" do
+        invalid_input = "fooo"
+        result = player.valid_input?(invalid_input)
+        expect(result).to be false
       end
-      it "reprompts words" do
-        # TODO
+      it "rejects numbers" do
+        invalid_input = "1234"
+        result = player.valid_input?(invalid_input)
+        expect(result).to be false
       end
-      it "reprompts invalid notations" do
-        # TODO
+      it "rejects invalid notations" do
+        invalid_input = "a1h9"
+        result = player.valid_input?(invalid_input)
+        expect(result).to be false
       end
-      it "reprompts correct notation that is more than 4 digits long" do
-        # TODO
+      it "rejects correct notation that is more than 4 digits long" do
+        invalid_input = "a1h8a1h8"
+        result = player.valid_input?(invalid_input)
+        expect(result).to be false
       end
-      it "reprompts keyword start" do
-        # TODO
+      it "rejects keyword start" do
+        invalid_input = "savec4d7"
+        result = player.valid_input?(invalid_input)
+        expect(result).to be false
       end
-      it "reprompts keyword end" do
-        # TODO
-      end
-      it "reprompts with multiple wrong inputs" do
-        # TODO
+      it "rejects keyword end" do
+        invalid_input = "h3b2save"
+        result = player.valid_input?(invalid_input)
+        expect(result).to be false
       end
     end
   end
