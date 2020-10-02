8c417c3b4cfe41af5b6eaeaed4465768e63204eb
chook <dylanw212@gmail.com>
2020-10-02 12:45:37 +1000

    fix tests after changes

--- spec/game_loop_spec.rb
+++ spec/game_loop_spec.rb
@@ -1,19 +1,19 @@
 require_relative "../lib/save_manager"
 require_relative "../lib/game_loop"
 
-describe Game do
-  subject(:game) { described_class.new(round: round) }
-  let(:round) { double("round") }
+describe GameLoop do
+  subject(:game) { described_class.new(round) }
+  let(:round) { double("round", draw_console: nil) }
 
   describe "#play" do
     it "loops correctly" do
-      allow(round).to receive(:gameover?).and_return(false, true)
+      allow(round).to receive(:game_over?).and_return(false, true)
       expect(round).to receive(:play).once
       game.play
     end
 
     it "works with a longer loop" do
-      allow(round).to receive(:gameover?).and_return(false, false, false, false, false, true)
+      allow(round).to receive(:game_over?).and_return(false, false, false, false, false, true)
       expect(round).to receive(:play).exactly(5).times
       game.play
     end
