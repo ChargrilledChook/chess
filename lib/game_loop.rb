cc4b80f9f1e4e49a8538862d9003454b4968d2b1
chook <dylanw212@gmail.com>
2020-10-02 12:46:12 +1000

    extract menu to main

--- lib/game_loop.rb
+++ lib/game_loop.rb
@@ -1,18 +1,9 @@
 # Controls gameplay flow
 class GameLoop
-  include SaveManager
-
   attr_reader :round
 
-  def initialize
-    @round = new_session
-  end
-
-  def new_session
-    # TODO: Extract string to display and polish implementation
-    puts "Welcome to Chess. Enter 1 for a new game or 2 to load your saved game."
-    selection = gets.chomp.downcase
-    selection == "2" ? load_save : Round.new
+  def initialize(round)
+    @round = round
   end
 
   def play
