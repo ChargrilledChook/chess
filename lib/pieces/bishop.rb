c75512fb2c67332b151a86933ac99bf6e07f238f
chook <dylanw212@gmail.com>
2020-10-06 19:18:51 +1100

    implement moving but not capturing

--- lib/pieces/bishop.rb
+++ lib/pieces/bishop.rb
@@ -1,17 +1,43 @@
 class Bishop
   include PieceIcons
 
+  MOVES = [[1, 1], [1, -1], [-1, -1], [-1, 1]].freeze
+
   attr_reader :colour
 
   def initialize(colour: :white)
     @colour = colour
   end
 
-  def moves
-    # TODO
+  def move_list(board, starting)
+    res = []
+    MOVES.each do |move|
+      begin
+        row = starting.first
+        col = starting.last
+        row += move.first
+        col += move.last
+        current = board.grid[row][col]
+        while valid?(row, col, current)
+          res << [row, col]
+          row += move.first
+          col += move.last
+          current = board.grid[row][col] if valid?(row, col, current)
+        end
+      rescue NoMethodError
+        next
+      end
+    end
+    res
   end
 
   def to_s
     colour == :white ? white_bishop : black_bishop
+  end
+
+  private
+
+  def valid?(row, col, current)
+    row.between?(0, 7) && col.between?(0, 7) && current == "   "
   end
 end
--- lib/pieces/rook.rb
+++ lib/pieces/rook.rb
@@ -1,17 +1,43 @@
 class Rook
   include PieceIcons
 
+  MOVES = [[1, 0], [-1, 0], [0, 1], [0, -1]].freeze
+
   attr_reader :colour
 
   def initialize(colour: :white)
     @colour = colour
   end
 
-  def moves
-    [[1, 0], [-1, 0], [0, 1], [0, -1]]
+  def move_list(board, starting)
+    res = []
+    MOVES.each do |move|
+      begin
+        row = starting.first
+        col = starting.last
+        row += move.first
+        col += move.last
+        current = board.grid[row][col]
+        while valid?(row, col, current)
+          res << [row, col]
+          row += move.first
+          col += move.last
+          current = board.grid[row][col] if valid?(row, col, current)
+        end
+      rescue NoMethodError
+        next
+      end
+    end
+    res
   end
 
   def to_s
     colour == :white ? white_rook : black_rook
+  end
+
+  private
+
+  def valid?(row, col, current)
+    row.between?(0, 7) && col.between?(0, 7) && current == "   "
   end
 end
