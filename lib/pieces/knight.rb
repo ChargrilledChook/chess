c66852dfb297d830ffeff039d1cdc720631c5d83
chook <dylanw212@gmail.com>
2020-10-07 13:27:57 +1100

    improve implementation of movelist

--- lib/pieces/knight.rb
+++ lib/pieces/knight.rb
@@ -12,13 +12,7 @@ class Knight < Piece
 
   # OPTIMISE: Unused params to be compatible with other pieces. Probably a better way to do this
   def move_list(_board, starting)
-    moves.map { |move|
-      begin
-        [starting.first + move.first, starting.last + move.last]
-      rescue NoMethodError
-        nil
-      end
-    }.compact
+    moves.map { |move| [starting.first + move.first, starting.last + move.last] }
   end
 
   def to_s
