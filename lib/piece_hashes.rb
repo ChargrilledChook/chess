051c0e69866539d775738a2b192d9b5deca269f8
chook <dylanw212@gmail.com>
2020-10-07 12:22:19 +1100

    complete default setup of a game

--- lib/piece_hashes.rb
+++ lib/piece_hashes.rb
@@ -1,5 +1,6 @@
 # Hashes of piece setups for the board class
 module PieceCollections
+  # OPTIMIZE: Should probably break this up into smaller chunks
   def default_pieces
     { [0, 0] => Rook.new(colour: :black),
       [0, 1] => Bishop.new(colour: :black),
@@ -16,7 +17,23 @@ module PieceCollections
       [7, 4] => King.new(colour: :white),
       [7, 5] => Knight.new(colour: :white),
       [7, 6] => Bishop.new(colour: :white),
-      [7, 7] => Rook.new(colour: :white) }
+      [7, 7] => Rook.new(colour: :white),
+      [6, 0] => Pawn.new(colour: :white),
+      [6, 1] => Pawn.new(colour: :white),
+      [6, 2] => Pawn.new(colour: :white),
+      [6, 3] => Pawn.new(colour: :white),
+      [6, 4] => Pawn.new(colour: :white),
+      [6, 5] => Pawn.new(colour: :white),
+      [6, 6] => Pawn.new(colour: :white),
+      [6, 7] => Pawn.new(colour: :white),
+      [1, 0] => Pawn.new(colour: :black),
+      [1, 1] => Pawn.new(colour: :black),
+      [1, 2] => Pawn.new(colour: :black),
+      [1, 3] => Pawn.new(colour: :black),
+      [1, 4] => Pawn.new(colour: :black),
+      [1, 5] => Pawn.new(colour: :black),
+      [1, 6] => Pawn.new(colour: :black),
+      [1, 7] => Pawn.new(colour: :black) }
   end
 
   def queens_test
