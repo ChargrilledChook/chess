0098713c75be87056d8502e775cb71880ec24b50
chook <dylanw212@gmail.com>
2020-10-14 10:51:11 +1100

    add to_s tests

--- spec/pieces/queen_spec.rb
+++ spec/pieces/queen_spec.rb
@@ -25,6 +25,20 @@ describe Queen do
   end
 
   describe "#to_s" do
+    context "when queen is white" do
+      subject(:white_queen) { described_class.new(colour: :white) }
 
+      it "returns the correct icon" do
+        expect(white_queen.to_s).to match "\u2655"
+      end
+    end
+
+    context "when queen is black" do
+      subject(:black_queen) { described_class.new(colour: :black) }
+
+      it "returns the correct icon" do
+        expect(black_queen.to_s).to match "\u265B"
+      end
+    end
   end
 end
