164e5ae7d0d7a93d1ff8a1cbc5fb7db05a3ec470
chook <dylanw212@gmail.com>
2020-10-13 16:55:32 +1100

    change formatting

--- spec/rook_spec.rb
+++ spec/rook_spec.rb
@@ -15,6 +15,7 @@ describe Rook do
         rook_moves = rook.move_list(board, [4, 5])
         expected = [3, 5], [2, 5], [1, 5], [0, 5], [5, 5], [6, 5], [7, 5],
                    [4, 4], [4, 3], [4, 2], [4, 1], [4, 0], [4, 6], [4, 7]
+
         expect(rook_moves).to match_array(expected)
       end
       it "is correct in the corners" do
@@ -22,6 +23,7 @@ describe Rook do
         rook_moves = rook.move_list(board, [0, 7])
         expected = [0, 0], [0, 1], [0, 2], [0, 3], [0, 4], [0, 5], [0, 6],
                    [1, 7], [2, 7], [3, 7], [4, 7], [5, 7], [6, 7], [7, 7]
+
         expect(rook_moves).to match_array(expected)
       end
       it "is correct in another corner" do
@@ -29,6 +31,7 @@ describe Rook do
         rook_moves = rook.move_list(board, [7, 7])
         expected = [7, 0], [7, 1], [7, 2], [7, 3], [7, 4], [7, 5], [7, 6],
                    [0, 7], [1, 7], [2, 7], [3, 7], [4, 7], [5, 7], [6, 7]
+
         expect(rook_moves).to match_array(expected)
       end
       it "is correct in another corner" do
@@ -36,6 +39,7 @@ describe Rook do
         rook_moves = rook.move_list(board, [7, 0])
         expected = [7, 1], [7, 2], [7, 3], [7, 4], [7, 5], [7, 6], [7, 7],
                    [0, 0], [1, 0], [2, 0], [3, 0], [4, 0], [5, 0], [6, 0]
+
         expect(rook_moves).to match_array(expected)
       end
     end
@@ -49,6 +53,7 @@ describe Rook do
         mini_board[0][2] = friend
         rook_moves = rook.move_list(mini_board, [0, 0])
         expected = [0, 1], [1, 0], [2, 0], [3, 0]
+
         expect(rook_moves).to match_array(expected)
       end
       it "is correct on the horiztonal" do
@@ -56,6 +61,7 @@ describe Rook do
         mini_board[1][3] = friend
         rook_moves = rook.move_list(mini_board, [0, 3])
         expected = [0, 0], [0, 1], [0, 2]
+
         expect(rook_moves).to match_array(expected)
       end
       it "is correct when blocked" do
@@ -64,6 +70,7 @@ describe Rook do
         mini_board[3][2] = friend
         rook_moves = rook.move_list(mini_board, [3, 3])
         expected = []
+
         expect(rook_moves).to match_array(expected)
       end
     end
@@ -76,6 +83,7 @@ describe Rook do
         mini_board[3][1] = enemy
         rook_moves = rook.move_list(mini_board, [1, 1])
         expected = [3, 1]
+
         expect(rook_moves).to include(expected)
       end
       it "works on a horizontal" do
@@ -83,6 +91,7 @@ describe Rook do
         mini_board[0][0] = enemy
         rook_moves = rook.move_list(mini_board, [0, 3])
         expected = [0, 0]
+
         expect(rook_moves).to include(expected)
       end
       it "works when adjacaent" do
@@ -91,6 +100,7 @@ describe Rook do
         mini_board[2][3] = enemy
         rook_moves = rook.move_list(mini_board, [3, 3])
         expected = [3, 2], [2, 3]
+
         expect(rook_moves).to match_array(expected)
       end
     end
@@ -109,6 +119,7 @@ describe Rook do
         board[2][5] = enemy
         rook_moves = rook.move_list(board, [2, 2])
         expected = [1, 2], [3, 2], [2, 3], [2, 4], [2, 5]
+
         expect(rook_moves).to match_array(expected)
       end
     end
