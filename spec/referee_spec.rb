3f1d16d8fa783a036c774e3f651690967b07dd19
chook <dylanw212@gmail.com>
2020-10-13 19:25:44 +1100

    add frame for ref spec

---
+++ spec/referee_spec.rb
@@ -0,0 +1,9 @@
+require_relative "./lib/referee"
+
+describe Referee do
+  subject(:ref) { described_class.new }
+  let(:enemy)   { double("enemy", colour: :black) }
+
+  describe "#check?" do
+  end
+end
