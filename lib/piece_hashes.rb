37be6734fdf1456e055f53256da4dddde8dc42f1
chook <dylanw212@gmail.com>
2020-10-13 11:07:06 +1100

    piece hashes broken into smaller chunks

--- lib/piece_hashes.rb
+++ lib/piece_hashes.rb
@@ -2,6 +2,12 @@
 module PieceCollections
   # OPTIMIZE: Should probably break this up into smaller chunks
   def default_pieces
+    black = big_black_pieces.merge(black_pawns)
+    white = big_white_pieces.merge(white_pawns)
+    black.merge(white)
+  end
+
+  def big_black_pieces
     { [0, 0] => Rook.new(colour: :black),
       [0, 1] => Bishop.new(colour: :black),
       [0, 2] => Knight.new(colour: :black),
@@ -9,24 +15,22 @@ module PieceCollections
       [0, 4] => King.new(colour: :black),
       [0, 5] => Knight.new(colour: :black),
       [0, 6] => Bishop.new(colour: :black),
-      [0, 7] => Rook.new(colour: :black),
-      [7, 0] => Rook.new(colour: :white),
+      [0, 7] => Rook.new(colour: :black) }
+  end
+
+  def big_white_pieces
+    { [7, 0] => Rook.new(colour: :white),
       [7, 1] => Bishop.new(colour: :white),
       [7, 2] => Knight.new(colour: :white),
       [7, 3] => Queen.new(colour: :white),
       [7, 4] => King.new(colour: :white),
       [7, 5] => Knight.new(colour: :white),
       [7, 6] => Bishop.new(colour: :white),
-      [7, 7] => Rook.new(colour: :white),
-      [6, 0] => Pawn.new(colour: :white),
-      [6, 1] => Pawn.new(colour: :white),
-      [6, 2] => Pawn.new(colour: :white),
-      [6, 3] => Pawn.new(colour: :white),
-      [6, 4] => Pawn.new(colour: :white),
-      [6, 5] => Pawn.new(colour: :white),
-      [6, 6] => Pawn.new(colour: :white),
-      [6, 7] => Pawn.new(colour: :white),
-      [1, 0] => Pawn.new(colour: :black),
+      [7, 7] => Rook.new(colour: :white) }
+  end
+
+  def black_pawns
+    { [1, 0] => Pawn.new(colour: :black),
       [1, 1] => Pawn.new(colour: :black),
       [1, 2] => Pawn.new(colour: :black),
       [1, 3] => Pawn.new(colour: :black),
@@ -36,22 +40,14 @@ module PieceCollections
       [1, 7] => Pawn.new(colour: :black) }
   end
 
-  def queens_test
-    { [0, 0] => Rook.new(colour: :black),
-      [0, 1] => Bishop.new(colour: :black),
-      [0, 2] => Knight.new(colour: :black),
-      [0, 3] => Queen.new(colour: :black),
-      [0, 4] => King.new(colour: :black),
-      [0, 5] => Knight.new(colour: :black),
-      [0, 6] => Bishop.new(colour: :black),
-      [0, 7] => Rook.new(colour: :black),
-      [7, 0] => Rook.new(colour: :white),
-      [7, 1] => Bishop.new(colour: :white),
-      [7, 2] => Knight.new(colour: :white),
-      [7, 3] => Queen.new(colour: :white),
-      [7, 4] => King.new(colour: :white),
-      [7, 5] => Knight.new(colour: :white),
-      [7, 6] => Bishop.new(colour: :white),
-      [7, 7] => Rook.new(colour: :white) }
+  def white_pawns
+    { [6, 0] => Pawn.new(colour: :white),
+      [6, 1] => Pawn.new(colour: :white),
+      [6, 2] => Pawn.new(colour: :white),
+      [6, 3] => Pawn.new(colour: :white),
+      [6, 4] => Pawn.new(colour: :white),
+      [6, 5] => Pawn.new(colour: :white),
+      [6, 6] => Pawn.new(colour: :white),
+      [6, 7] => Pawn.new(colour: :white) }
   end
 end
