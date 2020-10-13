1b7ecf015d6e0bde4b259793cad0920a5818c4f0
chook <dylanw212@gmail.com>
2020-10-13 19:25:56 +1100

    add experimental check method

--- lib/referee.rb
+++ lib/referee.rb
@@ -7,7 +7,9 @@ class Referee
     starting = move.starting
     ending = move.ending
     piece = board[starting.first][starting.last]
-    own_piece?(starting, board, player) && allowed_moves(piece, board, starting).include?(ending)
+    return false unless own_piece?(starting, board, player) && allowed_moves(piece, board, starting).include?(ending)
+
+    #check?(board, [7, 4]) == false
   end
 
   private
@@ -19,5 +21,21 @@ class Referee
 
   def allowed_moves(piece, board, starting)
     piece.move_list(board, starting)
+  end
+
+  # TODO: Currently untested and unimplemented, just gathering thoughts
+  # HACK: Prototype. Should break immediately upoun finding a check to optimize
+  def check?(board, king_pos)
+    check = false
+    king = board[king_pos.first][king_pos.last]
+    board.each_with_index do |rank, ridx|
+      rank.each_index do |file|
+        if board[ridx][file].colour == king.enemy_colour
+          piece = board[ridx][file]
+          check = true if allowed_moves(piece, board, [ridx, file]).include?(king_pos)
+        end
+      end
+    end
+    check
   end
 end
