2fd0eab655647bb93e022e4b61f503818af51ac7
chook <dylanw212@gmail.com>
2020-10-13 16:43:59 +1100

    add temporary template for piece specs

---
+++ spec/pieces/piece_template.rb
@@ -0,0 +1,32 @@
+require_relative "../lib/pieces/piece_icons"
+require_relative "../lib/pieces/piece"
+require_relative "../lib/pieces/INSERT HERE"
+
+# Temporary file to help with boilerplate for piece tests
+
+describe InsertPieceHere do
+  subject(:INSERTHERE) { described_class.new(colour: :white) }
+  let(:empty)          { double("empty", colour: :none) }
+
+  describe "#move_list" do
+    context "on an empty board" do
+
+    end
+
+    context "with friendly pieces" do
+
+    end
+
+    context "with enemy pieces" do
+
+    end
+
+    context "with enemies and friends" do
+
+    end
+  end
+
+  describe "#to_s" do
+
+  end
+end
