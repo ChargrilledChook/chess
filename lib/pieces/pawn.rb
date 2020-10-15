2b9ee57fbfa273862602898666286c4524aa238f
chook <dylanw212@gmail.com>
2020-10-15 12:54:13 +1100

    make first toggle a method

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -1,8 +1,8 @@
 class Pawn < Piece
   attr_reader :first_move
 
-  def post_initialize(first_move: true)
-    @first_move = first_move
+  def post_initialize
+    @first_move = true
   end
 
   # BUG: Unsuccessful move eats first move bonus
@@ -37,16 +37,22 @@ class Pawn < Piece
     res
   end
 
+  def toggle_first_move
+    @first_move = false
+  end
+
+  def to_s
+    colour == :white ? white_pawn : black_pawn
+  end
+
+  private
+
   def moves
     if first_move
       colour == :white ? [[-2, 0], [-1, 0]] : [[2, 0], [1, 0]]
     else
       colour == :white ? [[-1, 0]] : [[1, 0]]
     end
-  end
-
-  def to_s
-    colour == :white ? white_pawn : black_pawn
   end
 
   def attacks
