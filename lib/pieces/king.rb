21850e11d8f4f40a502d5bd335965e6ecdeb49fe
chook <dylanw212@gmail.com>
2020-10-15 16:19:21 +1100

    various helpers pushed to super + refactored

--- lib/pieces/king.rb
+++ lib/pieces/king.rb
@@ -13,24 +13,10 @@ class King < Piece
   # OPTIMISE: See Knight
   def move_list(board, starting)
     list = moves.map { |move| [starting.first + move.first, starting.last + move.last] }
-    list = list.select { |move| valid_move?(board, move) }
-    list.reject { |move| check_if_own_colour(board, move) }
+    list.select { |move| valid_move?(board, move) && !own_colour?(board, move) }
   end
 
   def to_s
     colour == :white ? white_king : black_king
-  end
-
-  private
-
-  # OPTIMISE: See #move_list
-  def valid_move?(board, move)
-    move.first.between?(0, board.size) && move.last.between?(0, board.size)
-  end
-
-  def check_if_own_colour(board, move)
-    board[move.first][move.last].colour == colour
-  rescue NoMethodError
-    true
   end
 end
--- lib/pieces/knight.rb
+++ lib/pieces/knight.rb
@@ -10,28 +10,13 @@ class Knight < Piece
      [1, 2]]
   end
 
-  # OPTIMISE: This code is currently duplicated in King. Probably a better way to deal with nil ranks
+  # OPTIMISE: This code is currently duplicated in King.
   def move_list(board, starting)
     list = moves.map { |move| [starting.first + move.first, starting.last + move.last] }
-    list = list.select { |move| valid_move?(board, move) }
-    list.reject { |move| check_if_own_colour(board, move) }
+    list.select { |move| valid_move?(board, move) && !own_colour?(board, move) }
   end
 
   def to_s
     colour == :white ? white_knight : black_knight
   end
-
-  private
-
-  # OPTIMISE: See #move_list
-  def valid_move?(board, move)
-    move.first.between?(0, board.size) && move.last.between?(0, board.size)
-  end
-
-  def check_if_own_colour(board, move)
-    board[move.first][move.last].colour == colour
-  rescue NoMethodError
-    true
-  end
 end
-
