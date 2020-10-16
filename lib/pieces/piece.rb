9771fc87b86d912880606a1bd1cb33d266f3008e
chook <dylanw212@gmail.com>
2020-10-16 15:32:33 +1100

    small refactor

--- lib/pieces/piece.rb
+++ lib/pieces/piece.rb
@@ -15,20 +15,17 @@ class Piece
   # HACK: Inefficent and ugly. Needs tests to refactor safely. It's possible some of these calculations
   # ie not being able to move through another piece don't belong here at all. This method may know too
   # much about other objects
+
   def move_list(board, starting)
     res = []
     moves.each do |move|
       begin
-        row = starting.first
-        col = starting.last
-        row += move.first
-        col += move.last
-        current = board[row][col]
-        while valid?(row, col, current)
+        row = starting.first + move.first
+        col = starting.last + move.last
+        while valid_move?(board, [row, col]) && empty?(board, [row, col])
           res << [row, col]
           row += move.first
           col += move.last
-          current = board[row][col] if valid?(row, col, current)
         end
         res << [row, col] if board[row][col].colour == enemy_colour
       rescue NoMethodError
@@ -37,6 +34,20 @@ class Piece
     end
     res
   end
+
+  # Protoype / experiment
+  # def move_map(board, starting, move)
+  #   row = starting.first + move.first
+  #   col = starting.last + move.last
+  #   while valid_move?(board, [row, col]) && empty?(board, [row, col])
+  #     res << [row, col]
+  #     row += move.first
+  #     col += move.last
+  #   end
+  #   res << [row, col] if board[row][col].colour == enemy_colour
+  # rescue NoMethodError
+  #   next
+  # end
 
   def moves
     raise NotImplementedError
@@ -47,13 +58,6 @@ class Piece
   end
 
   private
-
-  # HACK: This currently knows too much about board but is fine for now
-  # Valid and valid_move? are redundant but vestigial from initial implementation
-  # valid? can go once move_list is refactored
-  def valid?(row, col, current)
-    row.between?(0, 7) && col.between?(0, 7) && current.colour == :none
-  end
 
   def valid_move?(board, move)
     move.first.between?(0, board.size) && move.last.between?(0, board.size)
@@ -76,4 +80,6 @@ class Piece
   rescue NoMethodError
     true
   end
+
+
 end
