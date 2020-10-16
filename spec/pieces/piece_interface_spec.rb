7745dd74d90c9a0d7194ca5e18dbb53a3a47f7da
chook <dylanw212@gmail.com>
2020-10-16 13:14:06 +1100

    add piece interface tests

---
+++ spec/pieces/piece_interface_spec.rb
@@ -0,0 +1,17 @@
+#require_relative "../../lib/pieces/piece_icons"
+require_relative "../../lib/pieces/piece"
+
+# Unsure of the value of these tests but implementing as per the
+# inheritance testing advice of POODR. Simple enough to implement
+# and provides some documentation of the piece API
+describe Piece do
+  subject(:piece_interface) { described_class.new(colour: :white) }
+
+  context "with valid messages" do
+    it { is_expected.to respond_to :colour }
+    it { is_expected.to respond_to :post_initialize }
+    it { is_expected.to respond_to :move_list }
+    it { is_expected.to respond_to :moves }
+    it { is_expected.to respond_to :enemy_colour }
+  end
+end
