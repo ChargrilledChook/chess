657b54d3d48c67ba6de3f5b3efb322e66cecb356
chook <dylanw212@gmail.com>
2020-10-19 14:26:59 +1100

    add some place piece tests

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -8,14 +8,38 @@ describe Board do
       it "correctly updates the grid" do
         start = [0, 0]
         fin = [1, 1]
-        pos = board.grid[1][1]
+        end_pos = board.grid[1][1]
         board.place_move(start, fin)
-        expect(pos).to eq(0)
+        expect(end_pos).to eq(0)
       end
     end
   end
 
   describe "#place_pieces" do
+    context "with valid input" do
+      it "works with an empty hash" do
+        input = {}
+        board.place_pieces(input)
+        result = board.grid.all? { |rank| rank.all?(0) }
+
+        expect(result).to be true
+      end
+      it "works with a single entry" do
+        input = { [0, 0] => 5 }
+        result = board.grid
+        expected = [[5, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0]]
+        board.place_pieces(input)
+
+        expect(result).to eql expected
+      end
+    end
   end
 
   describe "#render_board" do
