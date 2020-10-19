c0073f65f76f3fb7b7c5ec78c9580b8528496518
chook <dylanw212@gmail.com>
2020-10-19 12:12:52 +1100

    add subject and first test

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,7 +1,13 @@
 require_relative "../lib/board"
 
 describe Board do
+  subject(:board) { described_class.new(nil, 0) }
+
   describe "#place_move" do
+    context "with valid input" do
+      it "correctly updates the grid" do
+      end
+    end
   end
 
   describe "#place_pieces" do
