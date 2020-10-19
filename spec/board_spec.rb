31f68440aa13d0f771c2cb03d3b9c75acdcafd64
chook <dylanw212@gmail.com>
2020-10-19 12:24:07 +1100

    first test passing

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,11 +1,16 @@
 require_relative "../lib/board"
 
 describe Board do
-  subject(:board) { described_class.new(nil, 0) }
+  subject(:board) { described_class.new({}, 0) }
 
   describe "#place_move" do
     context "with valid input" do
       it "correctly updates the grid" do
+        start = [0, 0]
+        fin = [1, 1]
+        pos = board.grid[1][1]
+        board.place_move(start, fin)
+        expect(pos).to eq(0)
       end
     end
   end
