86fe40aa240f9470c0f0e3145aa46641a76fd46d
chook <dylanw212@gmail.com>
2020-10-20 16:01:51 +1100

    add check method and various helpers

--- lib/referee.rb
+++ lib/referee.rb
@@ -1,19 +1,50 @@
+require "pry"
+
 class Referee
+  attr_reader :white_king, :black_king
+
   def initialize(board)
     @board = board
+    @white_king = board.grid[7][4]
+    @black_king = board.grid[0][4]
   end
 
   def valid_move?(move, board, player)
     starting = move.starting
     ending = move.ending
+    save_board_state(starting, ending)
     piece = board[starting.first][starting.last]
     return false unless own_piece?(starting, board, player) && allowed_moves(piece, board, starting).include?(ending)
 
-    #check?(board, [7, 4]) == false
     true
   end
 
+  # OPTIMIZE: Prototype. Refactor
+  def check?(board, player)
+    king = select_king(player)
+    check = false
+    board.each_with_index do |rank, ridx|
+      rank.each_index do |file|
+        if board[ridx][file].colour == king.enemy_colour
+          piece = board[ridx][file]
+          check = true if allowed_moves(piece, board, [ridx, file]).include?(king.position)
+        end
+      end
+    end
+    #binding.pry
+    check
+  end
+
+  def restore_board(starting, ending)
+    @board.grid[starting.first][starting.last] = @previous_from
+    @board.grid[ending.first][ending.last] = @previous_to
+  end
+
   private
+
+  def select_king(player)
+    player.colour == :white ? white_king : black_king
+  end
 
   def own_piece?(starting, board, player)
     piece = board[starting.first][starting.last]
@@ -24,19 +55,8 @@ class Referee
     piece.move_list(board, starting)
   end
 
-  # TODO: Currently untested and unimplemented, just gathering thoughts
-  # HACK: Prototype. Should break immediately upoun finding a check to optimize
-  def check?(board, king_pos)
-    check = false
-    king = board[king_pos.first][king_pos.last]
-    board.each_with_index do |rank, ridx|
-      rank.each_index do |file|
-        if board[ridx][file].colour == king.enemy_colour
-          piece = board[ridx][file]
-          check = true if allowed_moves(piece, board, [ridx, file]).include?(king_pos)
-        end
-      end
-    end
-    check
+  def save_board_state(starting, ending)
+    @previous_from = @board.grid[starting.first][starting.last]
+    @previous_to = @board.grid[ending.first][ending.last]
   end
 end
