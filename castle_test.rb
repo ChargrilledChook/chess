a4ce774a1bed313f62eaebe74dda92cfe2dc1903
chook <dylanw212@gmail.com>
2020-10-23 11:22:37 +1100

    add test prototypes for each castle possibilty

--- castle_test.rb
+++ castle_test.rb
@@ -1,26 +1,22 @@
 module Castle
-  def castle_long(grid, colour)
-    case colour
-    when :white
-      grid.place_move(piece_pos[:white_king], piece_to[:w_king_long])
-      grid.place_move(piece_pos[:w_long_rook], piece_to[:w_rook_long])
-    when :black
-      grid.place_move(piece_pos[:black_king], piece_to[:b_king_long])
-      grid.place_move(piece_pos[:b_long_rook], piece_to[:b_rook_long])
-    end
+  def castle_long_white(grid)
+    grid.place_move(piece_pos[:white_king], piece_to[:w_king_long])
+    grid.place_move(piece_pos[:w_long_rook], piece_to[:w_long_rook])
   end
 
-  def castle_short(grid, colour)
-    case colour
-    when :white
-      grid.place_move(piece_pos[:white_king], piece_to[:w_king_long])
-      grid.place_move(piece_pos[:white_king], piece_to[:w_king_long])
-    when :black
-      grid.place_move(piece_pos[:black_king], piece_to[:b_king_long])
-      grid.place_move(piece_pos[:black_king], piece_to[:b_king_long])
-    else
+  def castle_long_black(grid)
+    grid.place_move(piece_pos[:black_king], piece_to[:b_king_long])
+    grid.place_move(piece_pos[:b_long_rook], piece_to[:b_long_rook])
+  end
 
-    end
+  def castle_short_white(grid)
+    grid.place_move(piece_pos[:white_king], piece_to[:w_king_short])
+    grid.place_move(piece_pos[:w_short_rook], piece_to[:w_short_rook])
+  end
+
+  def castle_short_black(grid)
+    grid.place_move(piece_pos[:black_king], piece_to[:b_king_short])
+    grid.place_move(piece_pos[:b_short_rook], piece_to[:b_short_rook])
   end
 
   def piece_pos
@@ -35,11 +31,11 @@ module Castle
   def piece_to
     { w_king_long: [7, 2],
       w_king_short: [7, 6],
-      w_rook_long: [7, 3],
-      w_rook_short: [7, 5],
+      w_long_rook: [7, 3],
+      w_short_rook: [7, 5],
       b_king_long: [0, 2],
       b_king_short: [0, 6],
-      b_rook_long: [0, 3],
-      b_rook_short: [0, 5] }
+      b_long_rook: [0, 3],
+      b_short_rook: [0, 5] }
   end
 end
