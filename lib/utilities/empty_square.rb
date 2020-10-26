5846abd886b0274214cf4203b782d8f2184a68f4
chook <dylanw212@gmail.com>
2020-10-26 11:19:30 +1100

    move helper classes to a separate folder

--- lib/utilities/empty_square.rb
+++ lib/utilities/empty_square.rb
@@ -1,5 +1,5 @@
 # This class exists so that board grid co-ordinates can always be queried with colour
-# This simplifies many checks and caluclations for valid piece movement
+# This simplifies many checks and calculations for valid piece movement
 class EmptySquare
   def colour
     :none
