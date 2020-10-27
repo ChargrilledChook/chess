5f6ae118282a7c109c415dbc356a0dcd0d5112ea
chook <dylanw212@gmail.com>
2020-10-27 14:34:01 +1100

    Temp end game string

--- lib/game_loop.rb
+++ lib/game_loop.rb
@@ -10,5 +10,6 @@ class GameLoop
   def play
     round.draw_console
     round.play until round.game_over?
+    puts "#{round.players.last.colour.capitalize} wins by checkmate!" # TODO: TEMPORARY FOR TESTING ONLY
   end
 end
