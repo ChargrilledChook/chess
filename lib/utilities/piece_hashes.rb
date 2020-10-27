dda64f8dd666a9f33b7ecd77210ada8305c6979f
chook <dylanw212@gmail.com>
2020-10-27 14:34:11 +1100

    fix knight and bishop positions

--- lib/utilities/piece_hashes.rb
+++ lib/utilities/piece_hashes.rb
@@ -8,23 +8,23 @@ module PieceCollections
 
   def big_black_pieces
     { [0, 0] => Rook.new(colour: :black),
-      [0, 1] => Bishop.new(colour: :black),
-      [0, 2] => Knight.new(colour: :black),
+      [0, 2] => Bishop.new(colour: :black),
+      [0, 1] => Knight.new(colour: :black),
       [0, 3] => Queen.new(colour: :black),
       [0, 4] => King.new(colour: :black),
-      [0, 5] => Knight.new(colour: :black),
-      [0, 6] => Bishop.new(colour: :black),
+      [0, 5] => Bishop.new(colour: :black),
+      [0, 6] => Knight.new(colour: :black),
       [0, 7] => Rook.new(colour: :black) }
   end
 
   def big_white_pieces
     { [7, 0] => Rook.new(colour: :white),
-      [7, 1] => Bishop.new(colour: :white),
-      [7, 2] => Knight.new(colour: :white),
+      [7, 1] => Knight.new(colour: :white),
+      [7, 2] => Bishop.new(colour: :white),
       [7, 3] => Queen.new(colour: :white),
       [7, 4] => King.new(colour: :white),
-      [7, 5] => Knight.new(colour: :white),
-      [7, 6] => Bishop.new(colour: :white),
+      [7, 5] => Bishop.new(colour: :white),
+      [7, 6] => Knight.new(colour: :white),
       [7, 7] => Rook.new(colour: :white) }
   end
 
