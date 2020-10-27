1918bc6bce90e151117e151b9a806c09b5c5bf76
chook <dylanw212@gmail.com>
2020-10-27 14:34:39 +1100

    add convenience methods

--- lib/move_tree.rb
+++ lib/move_tree.rb
@@ -3,6 +3,21 @@
 class MoveTree
   attr_reader :board
 
+  def self.build_tree(board, colour)
+    board.grid.each_with_index.flat_map do |line, rank_idx|
+      line.each_index.filter_map do |file_idx|
+        [rank_idx, file_idx] if board.grid[rank_idx][file_idx].colour == colour
+      end
+    end
+  end
+
+  def self.build_move_lists(board, colour)
+    pieces = build_tree(colour)
+    pieces.each_with_object({}) do |piece, res|
+      res[piece] = board.grid[piece.first][piece.last].move_list(board.grid, [piece.first, piece.last])
+    end
+  end
+
   def initialize(board)
     @board = board
   end
@@ -19,6 +34,13 @@ class MoveTree
     pieces = build_tree(colour)
     pieces.each_with_object({}) do |piece, res|
       res[piece] = board.grid[piece.first][piece.last].move_list(board.grid, [piece.first, piece.last])
+    end
+  end
+
+  def convert_to_moves(colour)
+    move_list = build_move_lists(colour)
+    move_list.flat_map do |key, values|
+      values.map { |value| [key, value] }
     end
   end
 end
