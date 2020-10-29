24a15ac94675280d0ba2c7f0ef752671a24a8965
chook <dylanw212@gmail.com>
2020-10-30 10:52:12 +1100

    add castling dependency

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -1,5 +1,6 @@
 require_relative "../lib/utilities/piece_hashes"
 require_relative "../lib/utilities/save_manager"
+require_relative "../lib/castling"
 require_relative "../lib/round"
 
 describe Round do
