6f4e8b612fea67c0f270f823953324dd2afa5005
chook <dylanw212@gmail.com>
2020-11-03 17:12:03 +1100

    add dependencies

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -1,6 +1,7 @@
 require_relative "../lib/utilities/piece_hashes"
 require_relative "../lib/utilities/save_manager"
-require_relative "../lib/castling"
+require_relative "../lib/utilities/castling"
+require_relative "../lib/utilities/player_types"
 require_relative "../lib/round"
 
 describe Round do
