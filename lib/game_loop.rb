403d4c6c2b9bf6a7092a1d9e452513c951adc985
chook <dylanw212@gmail.com>
2020-11-03 16:25:16 +1100

    add top level comment

--- lib/game_loop.rb
+++ lib/game_loop.rb
@@ -1,4 +1,9 @@
-# Controls gameplay flow
+# GameLoop
+
+# Controls gameplay flow. Loops the game till it's over then prints the appropriate victory /
+# stalemate message. The main advantage of having a small looping class like this is that we
+# can inject a round object, which holds all data for gamestate. This allows for loading
+# saved game or preset board states.
 class GameLoop
   attr_reader :round
 
@@ -6,13 +11,13 @@ class GameLoop
     @round = round
   end
 
-  # TODO: Does draw console need to be here? Could simplify API
   def play
     round.boot_game
     round.play until game_over?
     post_game
   end
 
+  # TODO: Extract strings to display
   def post_game
     if round.checkmate?
       puts "\n#{round.players.last.colour.capitalize} wins by checkmate!"
